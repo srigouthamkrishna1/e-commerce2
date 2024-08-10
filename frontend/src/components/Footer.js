bedbc6133238d00de98567ca2167ecc865261ff2
srigouthamkrishna1 <srigouthamkrishna1@gmail.com>
2024-08-10 16:40:07 +0530

    fixed bug

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-slate-200 '>
-      <p className='text-center font-bold'>Gunna Bhavadeep@2024</p>
+      <p className='text-center font-bold'>Sri Goutham Krishna@2024</p>
     </footer>
   )
 }
